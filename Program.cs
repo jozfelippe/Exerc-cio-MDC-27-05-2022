55b759bc6b156f2a2086b6be23ba261f9fd5a413
JOSE FELIPE DA SILVA SANTOS <josefelipesilvasantos874@gmail.com>
2022-06-30 15:22:59 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Máximo Divisor Comum (método iterativo)");
+
+Console.Write("Digite o 1º número (a): ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o 2º número (b): ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int resto;
+do
+{
+    resto = a % b;
+    a = b;
+    b = resto;
+}
+while (resto != 0);
+
+Console.WriteLine($"MDC(a, b) = {a}");
